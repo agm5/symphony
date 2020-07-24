8ad23acb6052d1ea676c256de656eca67d8efa1b
agm5 <35504198+agm5@users.noreply.github.com>
2020-07-24 14:33:11 -0500

    Replace Symphony's j_V fit with Dexter 216 fit
    Still needs documentation and notes changes.  The function "maxwell_juettner_V()" has been changed to the j_V fit from Dexter 2016.  I've kept and renamed the former fitting function, "retired_maxwell_juettner_V()".  The new function needs to be tested more thoroughly still.

--- src/maxwell_juettner/maxwell_juettner_fits.c
+++ src/maxwell_juettner/maxwell_juettner_fits.c
@@ -67,7 +67,7 @@ double maxwell_juettner_Q(struct parameters * params)
 
 /*maxwell_juettner_V: fitting formula for the emissivity (polarized in Stokes V)
  *                    produced by a Maxwell-Juettner (relativistic thermal) 
- *                    distribution of electrons. (Eq. 29, 31 of [1])
+ *                    distribution of electrons. 
  *
  *@params: struct of parameters params
  *@returns: fit to the emissivity, polarized in Stokes V, for the given 
@@ -93,12 +93,45 @@ double maxwell_juettner_V(struct parameters * params)
                  *exp(-1.8899 * pow(X, 1./3.));
 	
   double ans = prefactor*term1*term2;
+	
+  return ans;                                                                                           
+  }
+
+/*maxwell_juettner_V: fitting formula for the emissivity (polarized in Stokes V)
+ *                    produced by a Maxwell-Juettner (relativistic thermal) 
+ *                    distribution of electrons. (Eq. 29, 31 of [1])
+ *
+ *@params: struct of parameters params
+ *@returns: fit to the emissivity, polarized in Stokes V, for the given 
+ *          parameters for a Maxwell-Juettner distribution.
+ */
+double retired_maxwell_juettner_V(struct parameters * params)
+{
+  double nu_c = get_nu_c(*params);
+
+  double nu_s = (2./9.)*nu_c*sin(params->observer_angle)*params->theta_e
+                *params->theta_e;
+
+  double X = params->nu/nu_s;
+
+  double prefactor = (params->electron_density 
+                      * pow(params->electron_charge, 2.) 
+                      * nu_c)/params->speed_light;
+
+  double term1 = (37.-87.*sin(params->observer_angle-28./25.))
+                /(100.*(params->theta_e+1.));
+
+  double term2 = pow(1.+(pow(params->theta_e, 3./5.)/25.+7./10.)
+		*pow(X, 9./25.), 5./3.);
+
+  double ans = prefactor*term1*term2*exp(-pow(X, 1./3.));
+
   /*NOTE: Sign corrected; the sign in Leung et al. (2011)
     and Pandya et al. (2016) for Stokes V transfer coefficients
     does not follow the convention the papers describe (IEEE/IAU);
-    the sign has been corrected here.*/ 
-  return ans;                                                                                           
-  }
+    the sign has been corrected here.*/
+  return ans;
+}
 
 /*planck_func: The Planck function (used in eq. 25 of [1]) can be used to
  *             obtain alpha_nu() fitting formulae from the j_nu() fitting 
